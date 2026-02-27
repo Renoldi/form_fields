b15e62df7a6abf186b0cd8d73d4d4dabb640a8d4
Renoldi <renoldiboz@gmail.com>
2026-02-27 13:39:49 +0700

    update

--- example/lib/config/app_routes.dart
+++ example/lib/config/app_routes.dart
@@ -2,40 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 enum AppRoute {
-  login(name: 'login', path: '/login', title: 'Login'),
-  menu(name: 'menu', path: '/menu', title: 'Menu'),
-  formFields(name: 'form_fields', path: '/form-fields', title: 'FormFields'),
-  dropdown(name: 'dropdown', path: '/dropdown', title: 'Dropdown'),
+  login(name: 'login', title: 'Login'),
+  menu(name: 'menu', title: 'Menu'),
+  formFields(name: 'form_fields', title: 'FormFields'),
+  dropdown(name: 'dropdown', title: 'Dropdown'),
   dropdownMulti(
     name: 'dropdown_multi',
-    path: '/dropdown-multi',
     title: 'Dropdown Multi',
   ),
-  radioButton(
-      name: 'radio_button', path: '/radio-button', title: 'Radio Button'),
-  checkbox(name: 'checkbox', path: '/checkbox', title: 'Checkbox'),
-  customClass(
-      name: 'custom_class', path: '/custom-class', title: 'Custom Class'),
-  validation(name: 'validation', path: '/validation', title: 'Validation'),
-  settings(name: 'settings', path: '/settings', title: 'Settings'),
-  profile(name: 'profile', path: '/profile', title: 'Profile'),
+  radioButton(name: 'radio_button', title: 'Radio Button'),
+  checkbox(name: 'checkbox', title: 'Checkbox'),
+  customClass(name: 'custom_class', title: 'Custom Class'),
+  validation(name: 'validation', title: 'Validation'),
+  settings(name: 'settings', title: 'Settings'),
+  profile(name: 'profile', title: 'Profile'),
   changePassword(
     name: 'change_password',
-    path: '/change-password',
     title: 'Change Password',
   ),
-  language(name: 'language', path: '/language', title: 'Language'),
-  appInfo(name: 'app_info', path: '/app-info', title: 'App Info');
+  language(name: 'language', title: 'Language'),
+  appInfo(name: 'app_info', title: 'App Info');
 
   final String name;
-  final String path;
   final String title;
 
   const AppRoute({
     required this.name,
-    required this.path,
     required this.title,
   });
+
+  String get path => '/${name.replaceAll('_', '-')}';
 }
 
 extension GoRouterExtensions on BuildContext {
