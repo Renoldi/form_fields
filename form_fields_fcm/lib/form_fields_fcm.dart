b3f028beede979d79daddce2c33bc963282452b9
Renoldi <renoldiboz@gmail.com>
2026-07-17 10:41:35 +0700

    update

--- form_fields_fcm/lib/form_fields_fcm.dart
+++
@@ -1,5 +0,0 @@
-// Re-export FCM helpers for consumers of the optional package.
-export 'src/service/fcm_service.dart'
-    show FCMService, fcmBackgroundHandler, FCMOptions;
-export 'model/fcm_models.dart'
-    show FCMMessage, FCMNotification, FCMMessageHandler;
